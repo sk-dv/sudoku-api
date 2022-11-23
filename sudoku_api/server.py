22edd72e5b7b40911594ceb6e4ebbc41e4b15cfa
skdv <felipe.osrn@gmail.com>
2022-11-22 21:28:10 -0600

    chore: added environment variables

--- sudoku_api/server.py
+++ sudoku_api/server.py
@@ -4,12 +4,15 @@ from strawberry.aiohttp.views import GraphQLView
 from graphql_schema import schema
 
 
+port = int(os.environ.get("PORT", 8080))
+graphiql = bool(os.environ.get("GRAPHIQL", True))
+
+
 async def app():
     app = web.Application()
-    app.router.add_route("*", "/graphql", GraphQLView(schema=schema, graphiql=False))
+    app.router.add_route("*", "/graphql", GraphQLView(schema=schema, graphiql=graphiql))
     return app
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
     web.run_app(app(), port=port)

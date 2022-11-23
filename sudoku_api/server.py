4728946fc50c872a8bcff73653fb85ad662a65ad
skdv <felipe.osrn@gmail.com>
2022-11-22 21:02:54 -0600

    chore: fixed server port

--- sudoku_api/server.py
+++ sudoku_api/server.py
@@ -1,10 +1,15 @@
+import os
 from aiohttp import web
 from strawberry.aiohttp.views import GraphQLView
 from graphql_schema import schema
 
-app = web.Application()
 
-app.router.add_route("*", "/graphql", GraphQLView(schema=schema))
+async def app():
+    app = web.Application()
+    app.router.add_route("*", "/graphql", GraphQLView(schema=schema))
+    return app
 
-if __name__ == '__main__':
-    web.run_app(app)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    web.run_app(app(), port=port)
